00a6fee5dcbe261385c5356313bb4bd6d19fa783
Narcelio <narcelio.slima@gmail.com>
2025-04-18 01:43:11 -0300

    Adjusting README.md, adding scrip of Greenbone Community Edition and DefectDojo

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# cybersecurity-labvm-pre-install
+# Cybersecurity LabVM Bootup
 Scripts para instalação do Greenbone Community Edition e do DefectDojo em uma VM Debian 12 (Bookworm).
+
+---
+
+## 📦 Requisitos
+
+Dispositivo com **[Debian 12 (Bookworm)](https://www.debian.org/releases/bookworm/)**
+
+---
+
+## Como utilizar?
+
+### 😎 Passo 1 - Logue como usuário Root
+
+```
+sudo su
+```
+
+### 🛠 Passo 2 - Atualize o Sistema
+
+```
+apt update
+```
+```
+apt upgrade
+```
+
+### 📝 Passo 3 - Baixe os Scripts, configure como executável e Execute
+
+#### 🦴 Greenbone Community Edition
+
+```
+wget https://raw.githubusercontent.com/Narceliolima/projeto-allura-fipeconsulter/master/openvas_install.sh && \
+	chmod +x openvas_install.sh
+./openvas_install.sh
+```
+
+#### 👩‍💻 DefectDojo
+
+```
+wget https://raw.githubusercontent.com/Narceliolima/projeto-allura-fipeconsulter/master/defectdojo_install.sh && \
+	chmod +x defectdojo_install.sh
+./defectdojo_install.sh
+```
+
+---
+
+## 💻 Tecnologias
+
+- **[Greenbone Community Edition](https://community.greenbone.net/)**
+- **[Docker](https://www.docker.com/)**
+- **[DefectDojo](https://defectdojo.com/community)**
+
+---
+
+## 🖇Referência
+
+ - [Script para instalação do Greenbone Community Edition no Debian 12 disponibilizado por Kastervo](https://github.com/Kastervo/OpenVAS-Installation)
---
+++ defectdojo_install.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# DefectDojo installation from sources for Debian 12 systems.
+# Run as root
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run this script as root."
+  exit
+fi
+
+
+# Install Docker
+echo "Installing Docker..."
+apt-get remove -y docker docker-engine docker.io containerd runc
+apt-get install -y ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
+
+# Install DefectDojo
+echo "Installing DefectDojo..."
+git clone https://github.com/DefectDojo/django-DefectDojo
+docker compose -f django-DefectDojo/docker-compose.yml build
+
+cat << EOF > defectdojo.service
+[Unit]
+Description=DefectDojo Service
+Requires=docker.service
+After=docker.service
+
+[Service]
+WorkingDirectory=/home/vagrant/django-DefectDojo
+ExecStart=/usr/bin/docker compose up
+ExecStop=/usr/bin/docker compose down
+TimeoutStartSec=0
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cp -v defectdojo.service /etc/systemd/system/
+
+#Instantiating and Starting Services
+systemctl daemon-reload
+systemctl enable defectdojo.service
+systemctl start defectdojo.service
